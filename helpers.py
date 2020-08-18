206a10ec1e555b28fb3a03a9b42688e9bfd73b10
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2020-08-18 16:57:26 -0700

    Added comment

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import request, redirect, session, url_for
 
+# Login required decorator
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
